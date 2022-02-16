acca5f4185b815e4ae703a33a9e38b5e1c87bcaf
saeromcho <saerom.r.cho@gmail.com>
2022-02-16 18:52:26 +0900

    build.gradle 세팅 추가 및 Application파일 생성

---
+++ src/main/java/com/rom/axon/AxonApplication.java
@@ -0,0 +1,11 @@
+package com.rom.axon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AxonApplication.class, args);
+    }
+}
